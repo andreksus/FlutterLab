fb3f32e57d23bd6655ccd0913ed70e315b18675c
Андрей Барышев <baryshev_2002@inbox.ru>
2023-12-10 20:20:30 +0600

    Added header: icons, profile image, username, tabbar

---
+++ lib/sections/header/user_name.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UserName extends StatelessWidget {
+  final String userName;
+  const UserName({super.key, required this.userName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 36, bottom: 14),
+      child: Text(
+      userName,
+      style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+      )
+    );
+  }
+}
